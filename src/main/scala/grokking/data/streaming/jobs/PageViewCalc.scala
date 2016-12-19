ad250d294345adf117a3158612f832a0b3328a21
vinhdangphuc <vinhdangphuc@gmail.com>
2016-12-19 23:44:08 +0700

    update streaming

--- src/main/scala/grokking/data/streaming/jobs/PageViewCalc.scala
+++ src/main/scala/grokking/data/streaming/jobs/PageViewCalc.scala
@@ -3,13 +3,12 @@ package grokking.data.streaming.jobs
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.StreamingContext
-import net.liftweb.json.DefaultFormats
-import grokking.data.utils.DateTimeUtils
-import org.apache.spark.streaming.kafka._
 import org.apache.spark.streaming.dstream._
-import kafka.serializer.StringDecoder
+import org.apache.spark.streaming.kafka._
+
 import grokking.data.streaming.redis.RedisClient
-import org.apache.spark.SparkConf
+import grokking.data.utils.DateTimeUtils
+import net.liftweb.json.DefaultFormats
 import redis.clients.jedis.Jedis
 
 object PageViewCalc {
@@ -19,11 +18,25 @@ object PageViewCalc {
     /**
      * Create DStream for specific kafka's topic
      */
-    def createKafkaStream(ssc: StreamingContext, kafkaTopic: String): DStream[(String, String)] = {
+    def createKafkaStream(ssc: StreamingContext, kafkaTopic: String, numberPartitions: Int): DStream[(String, String)] = {
         
+        /*val kafkaDStreams = (1 to numberPartitions).map { e => 
+            
+            
+        }
         KafkaUtils.createStream(
                 ssc, "s2:2181,s1:2181",
-                "group", Map(kafkaTopic -> 2)
+                "group", Map(kafkaTopic -> 4)
+            )
+        val unionDStream =  ssc.union(kafkaDStreams)
+        val processingParallelism = 4
+        val processingDStream = unionDStream.repartition(processingParallelism)
+        
+        unionDStream*/
+        
+        KafkaUtils.createStream(
+            ssc, "Team2-Server2:2181,Team2-Server1:2181",
+            "group", Map(kafkaTopic -> 4)
         )
     }
 
@@ -31,16 +44,16 @@ object PageViewCalc {
      *  Add uid to redis server using sorted set
      *  May be using HyperLogLog for fast and precision is not necessary
      */
-    def calcActiveUser(jedis: Jedis, key: String, uid: String, time: String): Unit = {
+    def putRedisSortedSet(jedis: Jedis, key: String, uid: String, score: String): Unit = {
 
-        jedis.zadd(key, time.toDouble, uid)
+        jedis.zadd(key, score.toDouble, uid)
     }
     
     /**
      * Video views by minute
      * Add videoId to redis server, using redis string
      */
-    def calcVideoViewByMinute(jedis: Jedis, key: String, videoId: String, time: String, expireSeconds: Int): Unit = {
+    def incrRedisKey(jedis: Jedis, key: String, time: String, expireSeconds: Int): Unit = {
         
         val videoViewKey = key + ":" + time
         
@@ -75,6 +88,20 @@ object PageViewCalc {
         jedis.zremrangeByScore(key, from, to)
     }
     
+    /**
+     * Using topvideos minute & hour to calculate topvideos
+     */
+    def rearrangeTopVideoView(jedis: Jedis, key: String, timestamp: Long): Unit = {
+        
+        var lstHourKeys = DateTimeUtils.takeListHourBefore(timestamp, 5)
+        lstHourKeys = lstHourKeys.map { hour => key + "_h:" + hour }
+        var lstMinuteKeys = DateTimeUtils.takeListMinute(timestamp, 5)
+        lstMinuteKeys = lstMinuteKeys.map { minute => key + "_m:" + minute }
+        var lstKeys = lstHourKeys ::: lstMinuteKeys
+        
+        jedis.zunionstore(key, lstKeys:_*)
+    }
+    
     def main(args: Array[String]){
         
         val prefix = args(0)
@@ -93,13 +120,14 @@ object PageViewCalc {
         val ssc = new StreamingContext(spark.sparkContext, Seconds(time.toInt))
         
         // create stream
-        val pageviewStream = createKafkaStream(ssc, prefix + "-pageview")
-        val clickStream = createKafkaStream(ssc, prefix + "-click")
-        val orderStream = createKafkaStream(ssc, prefix + "-order")
+        val pageviewStream = createKafkaStream(ssc, prefix + "-pageview", 4)
+        val clickStream = createKafkaStream(ssc, prefix + "-click", 4)
+        val orderStream = createKafkaStream(ssc, prefix + "-order", 4)
         
         // calculate page views and video views
         pageviewStream.foreachRDD(rdd => {
           
+            println("VINHDP: " + rdd.count)
             rdd.foreach(event => {
                 
                 // get event data
@@ -122,10 +150,10 @@ object PageViewCalc {
                 val minute = DateTimeUtils.format(timestamp, "yyyyMMddHHmm")
                 val hour = DateTimeUtils.format(timestamp, "yyyyMMddHH")
                 
-                calcActiveUser(jedis, "activeusers", uid, timestamp)    //manual expire item by timestamp score
-                calcVideoViewByMinute(jedis, "videoviews", videoId, minute, 1800)    // auto expire after 30 minutes
-                calcTopVideoView(jedis, "topvideos_min", videoId, minute, 21600)    // auto expire after 6 hours
-                calcTopVideoView(jedis, "topvideos_hour", videoId, hour, 21600)    // auto expire after 6 hours
+                putRedisSortedSet(jedis, key = "activeusers", uid, score = timestamp)    //manual expire item by timestamp score
+                incrRedisKey(jedis, "videoviews", minute, 1800)    // auto expire after 30 minutes
+                calcTopVideoView(jedis, "topvideos_m", videoId, minute, 18000)    // auto expire after 5 hours
+                calcTopVideoView(jedis, "topvideos_h", videoId, hour, 14400)    // auto expire after 4 hours
                 
                 // release redis resource
                 RedisClient.pool.returnResource(jedis)
@@ -141,13 +169,19 @@ object PageViewCalc {
             var endTime = (processTime - 360000).toString()
             expireSetItembyScore(jedis, "activeusers", "0", endTime)
             
+            /**
+             * calculate trending video
+             */
+            rearrangeTopVideoView(jedis, "topvideos", processTime)
+            
             RedisClient.pool.returnResource(jedis)
         })
         
         clickStream.foreachRDD(rdd => {
           
+            println("VINHDP: " + rdd.count)
             rdd.foreach(event => {
-                // get event data
+                
                 val timestamp = event._1
                 val json = event._2
                 implicit val formats = net.liftweb.json.DefaultFormats
@@ -155,22 +189,58 @@ object PageViewCalc {
                 // parse json string to object
                 val jsonObject = net.liftweb.json.parse(json).extract[ClickEvent]
                 val videoId = jsonObject.video
-                val userId = jsonObject.viewer.getOrElse("")
+                var uid = jsonObject.viewer
                 
-                // get redis resource
                 val jedis = RedisClient.pool.getResource
+                val minute = DateTimeUtils.format(timestamp, "yyyyMMddHHmm")
+                val hour = DateTimeUtils.format(timestamp, "yyyyMMddHH")
                 
-                val minutes = DateTimeUtils.format(timestamp, "yyyyMMddHHmm")
-                val clickKey = "clicks:" + minutes
+                incrRedisKey(jedis, "pageclicks", minute, 360)    // expire after 6 minutes
                 
-                jedis.zincrby(clickKey, 1, userId)
-                jedis.expire(clickKey, 600)    // expire after 10 minutes
-                
-                // release redis resource
                 RedisClient.pool.returnResource(jedis)
             })
         })
         
+        orderStream.foreachRDD(rdd => {
+          
+            println("VINHDP: " + rdd.count)
+            rdd.foreach(event => {
+                
+                val timestamp = event._1
+                val json = event._2
+                implicit val formats = net.liftweb.json.DefaultFormats
+                
+                // parse json string to object
+                val jsonObject = net.liftweb.json.parse(json).extract[OrderEvent]
+                val videoId = jsonObject.video
+                
+                var uid = jsonObject.viewer
+                
+                // get redis resource
+                val jedis = RedisClient.pool.getResource
+                val minute = DateTimeUtils.format(timestamp, "yyyyMMddHHmm")
+                val hour = DateTimeUtils.format(timestamp, "yyyyMMddHH")
+                
+                putRedisSortedSet(jedis, key = "orderusers", uid, score = timestamp)    //manual expire item by timestamp score
+                incrRedisKey(jedis, "orders", minute, 360)    // expire after 6 minutes
+                
+                // release redis resource
+                RedisClient.pool.returnResource(jedis)
+            })
+            
+            // clear redis sorted set from -inf to 5 minutes ago
+            val jedis = RedisClient.pool.getResource
+            
+            /**
+             * delete active user after 6 minutes programmatically (score is timestamp)
+             */
+            val processTime = System.currentTimeMillis();
+            var endTime = (processTime - 360000).toString()
+            expireSetItembyScore(jedis, "orderusers", "0", endTime)
+
+            RedisClient.pool.returnResource(jedis)
+        })
+        println("VINHDP: END")
         ssc.start()
         ssc.awaitTermination()
     }
@@ -180,3 +250,5 @@ case class PageViewEvent(referrer: String, viewer: Option[String], product: Stri
         video: String, uuid: String, url: String, timestamp: String)
 case class ClickEvent(referrer: String, viewer: Option[String], product: String, metric: String, location: String,
         video: String, uuid: String, url: String, timestamp: String)
+case class OrderEvent(referrer: String, viewer: String, product: String, metric: String, location: String,
+        video: String, uuid: String, url: String, order: String, timestamp: String)
